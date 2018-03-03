d7af3c9e090bf4113055a7f8ea184194962af2c8
thiago guedes <tguedes6@hotmail.com>
2018-03-03 15:30:46 -0300

    criado modulos pages e shared

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,23 +2,15 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { RegisterComponent } from './login/register.component';
 import { LoginComponent } from './login/login.component';
-import { PagesComponent } from './pages/pages.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 const routes: Routes = [
   // { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
 
+  // { path: '', redirectTo: 'pa', pathMatch: 'full' },
+
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: '', component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'grafica1', component: Graficas1Component },
-    ] },
   { path: '**', component: NopagefoundComponent },
 
 ];
---
+++ src/app/pages/pages.route.ts
@@ -0,0 +1,19 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+
+
+const routes: Routes = [
+  // { path: 'pa', component: PagesComponent,
+  { path: '', component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'grafica1', component: Graficas1Component },
+    ] },
+];
+
+export const PAGES_ROUTES = RouterModule.forChild( routes );
